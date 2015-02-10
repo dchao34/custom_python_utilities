fc56d411aedf68e922155dc747c42b2cb4caf7e9
dchao <dchao@caltech.edu>
2015-02-09 22:56:04 -0800

    Added function to get list of colors around HSV color wheel.

--- plot_format.py
+++ plot_format.py
@@ -1,9 +1,30 @@
+import colorsys
 from scipy.constants import golden
+import numpy as np
 import matplotlib.pyplot as plt
 
-hastie_colors = [ '#53ACCA', '#FFC466', '#FFAAAA', '#98D68E', '#CE89BE']
+hastie_colors = [ '#E69F00', '#56B4E9', '#009E73', '#F0E442', '#CC79A7', '#D55E00', '#0072B2' ]
+hastie_colors1 = [ '#53ACCA', '#FFC466', '#FFAAAA', '#98D68E', '#CE89BE']
+
 ggplot_colors = [ '#47ABAB', '#FFAAAA', '#FFCE6B', '#7C8BB0', '#FFFFAA', '#CE89BE', '#98D68E' ]
 ggoplot_dark_colors = [ '#088F8F', '#D46A6A', '#EFA30D', '#314577', '#DADA6D', '#8D3066', '#3F9232' ]
+
+# Get colors evenly distributed around the HSV color wheel.
+def get_hsv_colors(ncolors):
+
+    r_0, g_0, b_0 = 225 / 255., 101 / 255., 101 / 255.
+    h_0, s_0, v_0 = colorsys.rgb_to_hsv(r_0, g_0, b_0)
+    h_list = ((np.linspace(0, 1, ncolors + 1) + h_0) % 1.0).tolist()[:-1]
+
+    color_list = []
+    for h in h_list:
+        r, g, b = colorsys.hsv_to_rgb(h, s_0, v_0)
+        r, g, b = map(lambda x : int(255 * x), [r, g, b])
+        r, g, b = map(lambda x : hex(x)[2:], [r, g, b])
+        color_list.append('#' + r + g + b)
+
+    return color_list
+
 
 # Given an Axes object, format it into the style specified in this function.
 def format_axes(ax, tick_fontsize=18, spines=False):
